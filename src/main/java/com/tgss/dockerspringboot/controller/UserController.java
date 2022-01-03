6bfe4fbc13fda0430cdb6d050f284c8bfade3bd7
HP <ali.bahadar03@gmail.com>
2022-01-03 18:42:59 +0500

    second commit

--- src/main/java/com/tgss/dockerspringboot/controller/UserController.java
+++ src/main/java/com/tgss/dockerspringboot/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class UserController {
     @GetMapping("/hi")
     public String hello(){
         return  "Hello my first docker spring boot app on jenkins";
